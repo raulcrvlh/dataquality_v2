d5722ac394075243cec7093d951c7524f94db18e
Raul Felipe Carvalho <raulf.crvlh@gmail.com>
2024-09-28 18:04:22 -0300

    numerical and categorical analyzes

--- dataquality.py
+++ dataquality.py
@@ -9,17 +9,10 @@ class DataQuality:
         self.df_cat = self.df.select_dtypes(exclude=np.number)
         self.df_num = self.df.select_dtypes(include=np.number)
 
-    def header(self, n=5):
+    def firts_rows(self, n=5):
         print("DataFrame Completo:")
         head_df = self.df.head(n)
         print(tabulate(head_df, headers="keys", showindex="always", tablefmt="fancy_grid"))
-        # total_linhas = len(self.df)
-        # num_colunas = len(self.df.columns)
-        # total_nulos = self.df.isna().sum().reset_index()
-
-        # print(f"\nNúmero total de linhas: {total_linhas}")
-        # print(f"\nNúmero de colunas: {num_colunas}")
-        # print(f"\nTotal de dados nulos: {total_nulos}")
 
         print("\nDataFrame Categórico:")
         categ_df = self.df_cat.head(n)
@@ -29,6 +22,19 @@ class DataQuality:
         num_df = self.df_num.head(n)
         print(tabulate(num_df, headers="keys", showindex="always", tablefmt="fancy_grid"))
     
+    def last_rows(self, n=5):
+        print("\nDataFrame Completo:")
+        tail_df = self.df.tail(n)
+        print(tabulate(tail_df, headers="keys", showindex="always", tablefmt="fancy_grid"))
+
+        print("\nDataFrame Categórico:")
+        categ_df = self.df_cat.tail(n)
+        print(tabulate(categ_df, headers="keys", showindex="always", tablefmt="fancy_grid"))
+
+        print("\nDataFrame Numérico:")
+        num_df = self.df_num.tail(n)
+        print(tabulate(num_df, headers="keys", showindex="always", tablefmt="fancy_grid"))
+
     def count_nulls(self):
         print("\nQuantidade de Nulos:")
         df_nulos= self.df.isnull().sum().reset_index()
@@ -47,10 +53,16 @@ class DataQuality:
             df_aux = self.df_cat[col].mode()
             print(f"{col}: {df_aux[0]}")
 
-    def df_describe(self):
+    def numerical_analyzes(self):
         for col in self.df_num:
             print(f"\n{col} - Coluna Númerica")
-            df_num_descbrie = self.df_num[col].describe().reset_index()
+            df_num_descbrie = self.df_num[col].describe().round(2).reset_index()
             print(tabulate(df_num_descbrie, headers="keys", tablefmt="fancy_grid"))
             self.df_num[col].plot(subplots=True, kind='bar', figsize=(25,10), fontsize=5)
             plt.show()
+
+    def categorical_analyzes(self):
+        for col in self.df_cat:
+            print(f"\n{col} - Coluna Categórica")
+            df_aux = self.df_cat[col].value_counts().reset_index()
+            print(tabulate(df_aux, headers="keys", tablefmt="fancy_grid"))
